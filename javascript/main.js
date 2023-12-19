edf817cf32abcab5d4798de041d4edd08956074b
AbdelrahmanNijim <146373528+AbdelrahmanNijim@users.noreply.github.com>
2023-12-19 06:12:31 +0200

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,3 @@
+document.getElementById("div1").classList.add("demo");
+document.getElementById("div1").textContent="the demo class added from js ";
+document.querySelector(".vip").classList.remove("vip")
