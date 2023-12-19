71435b2e5a7cc95d99b262082801e2334028cc51
AbdelrahmanNijim <146373528+AbdelrahmanNijim@users.noreply.github.com>
2023-12-19 06:13:22 +0200

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,3 @@
 document.getElementById("div1").classList.add("demo");
-document.getElementById("div1").textContent="the demo class added from js ";
+document.getElementById("div1").textContent="the demo class and the text added from js ";
 document.querySelector(".vip").classList.remove("vip")
